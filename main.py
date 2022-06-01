957f13eb70b93603ea90c007a672ae44f8368a75
Jack Tollenaar <95088427+190416@users.noreply.github.com>
2022-06-01 08:28:52 +0000

    Pijn

--- main.py
+++ main.py
@@ -1,9 +1,88 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3 as sql
+import requests
+import json
+import json2table
+
 
 app = Flask(__name__)
 
+#Get json data
+response_API = requests.post ('https://po-meten.arhc.informatica.gsf.nl/4-logs-api.php', data = {"req":"getall"})
+data = response_API.text
+
+print ("data:", data)
+
+parse_json = json.loads(data)
+
+
+
+
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def home():
+    return render_template('home.html')
+
+@app.route('/jsontable')
+def jsonlist():
+  build_direction = "LEFT_TO_RIGHT"
+  table_attributes = {"style": "width:100%"}
+  return json2table.convert(parse_json, 
+                         build_direction=build_direction, 
+                         table_attributes=table_attributes)
+
+
+@app.route('/enternew')
+def new_student():
+    return render_template('student.html')
+
+
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            nm = request.form['nm']
+            addr = request.form['add']
+            city = request.form['city']
+            pin = request.form['pin']
+
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+              
+                cur.execute(
+                    "INSERT INTO students (name,addr,city,pin)                VALUES (?,?,?,?)",
+                    (nm, addr, city, pin))
+
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+        finally:
+            return render_template("result.html", msg=msg)
+            con.close()
+
+
+@app.route('/list')
+def list():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from students")
+
+    rows = cur.fetchall()
+    return render_template("list.html", rows=rows)
+
+
+@app.route('/init')
+def init_database():
+    conn = sql.connect('database.db')
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)'
+    )
+    conn.close()
+    return render_template("done.html", msg="Database tabel created")
+
 
 app.run(host='0.0.0.0', port=81)
